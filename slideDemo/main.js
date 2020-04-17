3ac226c98eb1a840ec4b95a79489f00440e12537
zhangxiang <1203901485@qq.com>
2020-04-18 00:55:15 +0800

    实现无缝轮播项目！

---
+++ slideDemo/main.js
@@ -0,0 +1,23 @@
+init()
+
+let n = 1
+setInterval(()=>{
+    $(`.images > img:nth-child(${x(n)})`).removeClass('current').addClass('leave')
+    .one('transitionend',(e)=>{
+        $(e.currentTarget).removeClass("leave").addClass('enter')
+    })
+    $(`.images > img:nth-child(${x(n+1)})`).removeClass('enter').addClass("current")
+    n += 1
+}, 3000)
+
+function x(n){
+    return n = (n%3!==0) ? n%3 : 3
+}
+
+// 初始化状态
+function init(){
+    $('.images > img:nth-child(1)').addClass('current')
+    $('.images > img:nth-child(2)').addClass('enter')
+    $('.images > img:nth-child(3)').addClass('enter')
+}
+
