572a87c2aca4c1c1622c519f738aee317b068937
zhangxiang <1203901485@qq.com>
2020-04-26 23:06:56 +0800

    修复bug：解决来页面切换时不能正常显示的问题！

--- slideDemo/main.js
+++ slideDemo/main.js
@@ -1,7 +1,7 @@
 init()
 
 let n = 1
-setInterval(()=>{
+let timer = setInterval(()=>{
     $(`.images > img:nth-child(${x(n)})`).removeClass('current').addClass('leave')
     .one('transitionend',(e)=>{
         $(e.currentTarget).removeClass("leave").addClass('enter')
@@ -9,6 +9,25 @@ setInterval(()=>{
     $(`.images > img:nth-child(${x(n+1)})`).removeClass('enter').addClass("current")
     n += 1
 }, 3000)
+
+// 当切换至别的页面时清空闹钟，待切回到页面之后继续轮播
+document.addEventListener("visibilitychange", function(e){
+    if(document.hidden){
+        window.clearInterval(timer)
+    }else{
+        timer = setInterval(()=>{
+            $(`.images > img:nth-child(${x(n)})`).removeClass('current').addClass('leave')
+            .one('transitionend',(e)=>{
+                $(e.currentTarget).removeClass("leave").addClass('enter')
+            })
+            $(`.images > img:nth-child(${x(n+1)})`).removeClass('enter').addClass("current")
+            n += 1
+        }, 3000)
+    }
+})
+
+
+
 
 function x(n){
     return n = (n%3!==0) ? n%3 : 3
